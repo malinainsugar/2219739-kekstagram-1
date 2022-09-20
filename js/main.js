3a39484da793a0f83ff0ae0237411323284d2ce5
malinainsugar <super.a03-a2013@yandex.ru>
2022-09-20 20:00:38 +0500

    aaa

--- js/main.js
+++ js/main.js
@@ -1,17 +1,2 @@
-// eslint-disable-next-line no-unused-vars
-function getRandomNumber (minimum, maximum) {
-  if (maximum >= minimum && minimum >= 0) {
-    return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-  } else {
-    return -1;
-  }
-}
-// eslint-disable-next-line no-unused-vars
-function checkLengthString (line, maximum) {
-  const stringLength = line.length;
-  if (stringLength <= maximum) {
-    return true;
-  }
-  return false;
-}
+
 
