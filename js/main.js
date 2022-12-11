ea81a187bd1a076f2dd1d5dc67b710ca038224c5
malinainsugar <super.a03-a2013@yandex.ru>
2022-12-11 22:59:13 +0500

    Добавление фильтрации изображений от других пользователей

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
-
-import { renderSimilarList } from './rendering-mini.js';
-import { setUserFormSubmit, closeEditingWindow } from './form-script.js';
-import { getDataFromServer } from './api.js';
-import { showAlert } from './util.js';
-
-getDataFromServer(
-  (photos) => {
-    renderSimilarList(photos);
-  },
-  (message) => {
-    showAlert(message);
-  },);
-
-setUserFormSubmit(closeEditingWindow);
+import './rendering-photos.js'
--- js/util.js
+++ js/util.js
@@ -49,4 +49,12 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-export {getRandomArrayElement, getRandomNumber, isEscapeKey, checkForRepeats, showAlert};
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+export {getRandomArrayElement, getRandomNumber, isEscapeKey, checkForRepeats, showAlert, debounce};
